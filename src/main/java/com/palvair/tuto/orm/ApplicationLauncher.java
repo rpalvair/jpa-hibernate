ca1bf1183284532aa3b1b405ece8c7a84270ab63
ruddy <rpalvair@WO-RPALVAIR-01.wideorbit.com>
2014-10-08 14:20:29 +0200

    Added conf for JPA

--- src/main/java/com/palvair/tuto/orm/ApplicationLauncher.java
+++ src/main/java/com/palvair/tuto/orm/ApplicationLauncher.java
@@ -1,10 +1,15 @@
 package com.palvair.tuto.orm;
 
 import lombok.extern.log4j.Log4j;
+import org.hibernate.ejb.HibernatePersistence;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 /**
  * Created by rpalvair on 07/10/2014.
@@ -14,8 +19,43 @@ import org.springframework.context.annotation.Configuration;
 public class ApplicationLauncher {
 
     @Bean
-    OrmBean ormBean() {
+    public OrmBean ormBean() {
         return new OrmBean();
+    }
+
+    @Bean
+    public javax.sql.DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUsername("root");
+        dataSource.setPassword("");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/tutoorm");
+        return dataSource;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+        entityManagerFactoryBean.setDataSource(dataSource());
+        //entityManagerFactoryBean.setPersistenceUnitName("");
+        entityManagerFactoryBean.setPackagesToScan("com.palvair.tuto.orm.entity");
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        vendorAdapter.setDatabasePlatform("org.hibernate.dialect.MySQL5Dialect");
+        vendorAdapter.setShowSql(true);
+        vendorAdapter.setGenerateDdl(true);
+
+        entityManagerFactoryBean.setJpaVendorAdapter(vendorAdapter);
+        entityManagerFactoryBean.setPersistenceProviderClass(HibernatePersistence.class);
+        return entityManagerFactoryBean;
+    }
+
+    @Bean
+    public JpaTransactionManager transactionManager() throws ClassNotFoundException {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+
+        transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
+
+        return transactionManager;
     }
 
     public static void main(String[] args) {
